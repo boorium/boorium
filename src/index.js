5b92eec99c35a5aad939d07110eda047e5aecfa4
ivan <ivan@sq10.net>
2016-04-05 22:01:56 -0500

    barebones electron app, update standard to liking

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 'use strict'
 
-console.log('hello world')
+import {app, BrowserWindow} from 'electron'
+import {join} from 'path'
 
+let mainWindow
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.on('ready', async () => {
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 600
+  })
+
+  mainWindow.loadURL(`file://${join(__dirname, 'index.html')}`)
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', async () => {
+    mainWindow = null
+  })
+})
